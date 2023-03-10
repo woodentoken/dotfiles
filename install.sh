4d9985615e06162017ef679135e90b615ba9009e
Kaleb Bordner <kaleb.bordner@gmail.com>
2023-03-10 11:32:00 -0800

    install wemux

--- install.sh
+++ install.sh
@@ -80,6 +80,15 @@ log "...Done"
 
 
 #################################################
+### {WEMUX}
+git clone https://github.com/zolrath/wemux.git /usr/local/share/wemux
+ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
+cp /usr/local/share/wemux/wemux.conf.example /usr/local/etc/wemux.conf
+echo "host_list=(kbordner $USER)" >> /usr/local/etc/wemux.conf
+#################################################
+
+
+#################################################
 ### {VIM} Install Plug
 log "Installing Vim Plug plugin manager..."
 curl -fLo ./vim/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> $LOG
