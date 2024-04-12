7f629b2acde0f4a2526cc5bc643fbbae7cbe38db
Kaleb Bordner <kaleb.bordner@gmail.com>
2024-04-11 17:56:00 -0700

    adding ranger

--- install.sh
+++ install.sh
@@ -15,81 +15,14 @@ function log {
 	fi
 }
 
-# WIP
-# # find all dotfiles in the home directory and remove them
-# function find_conflicting_dotfiles {
-#   incoming_files=()
-#   while IFS= read -r -d $'\0'; do
-#     incoming_files+=("$REPLY")
-#   done < <(find . -maxdepth 2 -type f,d -print0)
-
-#   existing_files=()
-#   while IFS= read -r -d $'\0'; do
-#     existing_files+=("$REPLY")
-#   done < <(find ../.* -maxdepth 0 -type f,d -print0)
-
-#   # basenameify existing_file dotfiles
-#   for i in {1..$#existing_files}; do
-#     existing_files[$i]=$(basename ${existing_files[$i]})
-#   done
-
-#   # basenameify incoming_filesoming dotfiles
-#   for i in {1..$#incoming_files}; do
-#     incoming_files[$i]=$(basename ${incoming_files[$i]})
-#   done
-
-#   # compute the intersection of the basenamed files
-#   common_dotfiles=($(comm -12 <(printf '%s\n' "${incoming_files[@]}" | sort) <(printf '%s\n' "${existing_files[@]}" | sort)))
-# }
-
-# common_dotfiles=$(find_conflicting_dotfiles)
-
-# # if conflicting dotfiles exist, ask how we should handle them
-# if [ ! -z $common_dotfiles ]; then
-#   echo "conflicts found in home directory: $common_dotfiles."
-#   echo "How do you want to handle conflicting files/folders that are currently in your home directory? (b/d/n)"
-#   select bdn in "create backups" "delete them" "do nothing"; do
-#     case $bdn in
-#       "create backups")
-#         # backup existing files
-#         backup_folder=~/.dotfile_backup_$(date '+%Y_%m_%d_%H_%M_%S')
-#         mkdir $backup_folder
-
-#       # move conflicting files into a backup folder
-#       pushd ~
-#       mv ${common_dotfiles[@]} $backup_folder
-#       popd
-
-#       unset IFS
-
-#       # locate existing_files dotfiles that may conflict
-#       log "moved ${common_dotfiles[@]} into $backup_folder"
-
-#       break
-#       ;;
-#     "delete them")
-#       log "deleting ${common_dotfiles[@]}"
-
-#       pushd ~
-#       rm -rf ${common_dotfiles[@]}
-#       popd
-#       break
-#       ;;
-#     "do nothing")
-#       log 'did not touch existing files, you *may* encounter issues with installation and symlinking as a result'
-#       break
-#     esac
-#   done
-# fi
-#
-
 # move possibly conflicting dotfiles into a backup directory
-cd ~
+pushd ~
 mkdir dotfiles_old
 mv .bashrc dotfiles_old
 mv .profile dotfiles_old
 mv .vimrc dotfiles_old
 mv .bash_logout dotfiles_old
+popd
 
 #################################################
 ### Basics
@@ -115,6 +48,7 @@ dotfile_packages="
   python3-pip
   python3-setuptools
   r-base
+  ranger
   redshift
   stow
   sxhkd
@@ -207,13 +141,17 @@ log "...Done"
 
 #################################################
 ### {Rust} Install Rust
+log "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+log "...Done"
 #################################################
 
 
 #################################################
 ### {Alacritty} Install Alacritty
+log "Installing Alacritty"
 cargo install alacritty
+log "...Done"
 #################################################
 
 
@@ -223,14 +161,6 @@ log "Installing fzf..."
 run git clone --depth 1 https://github.com/junegunn/fzf.git ./fzf/.fzf
 # auto answer prompts with autocompletion y, key-bindings y, update-config n
 echo 'y y n' | run ./fzf/.fzf/install --no-bash
-log "...Done"
-#################################################
-
-
-#################################################
-### {ZSH} install zsh plugins
-log "Installing zsh plugins..."
-run ./zsh/resolve_zsh_plugins.sh
 log "...Done"
 #################################################
 
@@ -286,15 +216,21 @@ for directory in */; do
 	stow --adopt -R $directory
 	echo "  ${directory} stowed in ${HOME}"
 done
+#################################################
 
-# install some zsh plugins
-~/resolve_zsh_plugins.sh
+
+#################################################
+### {ZSH} install zsh plugins
+log "Installing zsh plugins..."
+cd ~
+run ./resolve_zsh_plugins.sh
+log "...Done"
 #################################################
 
 
 #################################################
 ### {ZSH} make zsh default shell
-log "Making zsh default shell..."
+log "Making zsh default shell for ROOT and current USER..."
 sudo chsh -s $(`which zsh`)
 chsh -s $(`which zsh`) $USER
 log "...Done"
@@ -304,5 +240,7 @@ log "...Done"
 sudo apt-get autoremove
 zsrc
 
-log "All Done!"
+log "All Done! Rebooting your computer is recommended!"
+log "Exiting..."
+
 exit 0
