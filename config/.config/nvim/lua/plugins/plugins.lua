6d1b5c59d83e06a6583584adc60006ca35f90846
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-07-31 13:50:02 -0400

    updates to nvim

--- config/.config/nvim/lua/plugins/plugins.lua
+++ config/.config/nvim/lua/plugins/plugins.lua
@@ -49,4 +49,12 @@ return {
       vim.notify = require("notify")
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.highlight = opts.highlight or {}
+      opts.highlight.enable = true
+      opts.highlight.disable = { "html", "htmldjango" }
+    end,
+  },
 }
