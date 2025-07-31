1b173b2de75c9727cb527ebb397aea595dedb747
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-07-31 16:54:47 -0400

    add neovim to install script

--- install.sh
+++ install.sh
@@ -191,6 +191,13 @@ log "...Done"
 #################################################
 
 #################################################
+### {neovim} Install the latest version of neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+#################################################
+
+#################################################
 ### {Rust} Install Rust
 log "Installing rust(up)..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
