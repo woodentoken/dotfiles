ed6d0a545cc4a7d41cc251418b7a9118f6943e33
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-08-13 16:52:48 -0400

    additional stuff

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -18,3 +18,10 @@ vim.api.nvim_create_autocmd("User", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
