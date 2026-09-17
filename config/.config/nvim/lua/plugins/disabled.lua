e88d282b7aeaa7678a70d4a4fddb2b1dd653be2d
Kaleb Bordner <kaleb.bordner@gmail.com>
2026-09-17 10:41:14 -0400

    nvim: removing auto quotes
    altering surround

--- config/.config/nvim/lua/plugins/disabled.lua
+++ config/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,8 @@
 return {
-  { "folke/noice.nvim", enabled = false },
+  { "folke/noice.nvim",                    enabled = false },
   -- { "folke/trouble.nvim", enabled = false },
   -- { "folke/flash.nvim", enabled = false },
-  { "m4xshen/hardtime.nvim", enabled = false },
+  { "m4xshen/hardtime.nvim",               enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  -- { "echasnovski/mini.pairs",              enabled = false },
 }
