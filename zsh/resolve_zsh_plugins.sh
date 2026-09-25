fb28099cedb7f79bf64f804bd8d4e6eb41bfa23b
Kaleb Bordner <kaleb.bordner@gmail.com>
2026-09-25 13:23:26 -0400

    zsh: remove zsh-bd, was not using it often

--- zsh/resolve_zsh_plugins.sh
+++ zsh/resolve_zsh_plugins.sh
@@ -22,7 +22,6 @@ function update_or_install {
 }
 
 update_or_install "KulkarniKaustubh/fzf-dir-navigator"
-update_or_install "Tarrasch/zsh-bd"
 update_or_install "ael-code/zsh-colored-man-pages"
 update_or_install "agkozak/zsh-z"
 update_or_install "djui/alias-tips"
